f090f5bb560dac36d7570a5f48bd5b4177b7ffce
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-04 23:08:28 -0300

    feat: add styles loader

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
-export default function Loading() {
-  return <p>Carregando...</p>
+import { Skeleton } from '@/components/skeleton'
+
+export default function HomeLoading() {
+  return (
+    <div className="grid-rows-6 grid h-full grid-cols-9 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[856px] w-full" />
+      <Skeleton className="col-span-3 row-span-3 w-full" />
+      <Skeleton className="col-span-3 row-span-3 w-full" />
+    </div>
+  )
 }

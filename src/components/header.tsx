6c27d28f0ffbd1a0f7c137f2e7dd7667fcf726e3
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-08 17:27:38 -0300

    refactor: remove form and cart

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
-import { Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import CartWidget from './cart-widget'
+import { SearchForm } from './search-form'
 
 export default function Header() {
   return (
@@ -19,22 +20,10 @@ export default function Header() {
           </span>
         </Link>
 
-        <form className="group flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700 focus-within:ring-1 focus-within:ring-violet-500">
-          <Search className="size-5 text-violet-500" />
-
-          <input
-            type="text"
-            placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="size-4" />
-          <span className="text-sm">Cart (0)</span>
-        </div>
-
+        <CartWidget />
         <div className="h-4 w-px bg-zinc-700" />
 
         <Link href={'/'} className="flex items-center gap-2 hover:underline">

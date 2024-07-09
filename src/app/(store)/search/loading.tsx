f9fa475db7e7022661ef8f1dbc1baf646dfe7bb4
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-08 22:33:42 -0300

    refactor: remove suspense

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -2,25 +2,22 @@
 
 import { Skeleton } from '@/components/skeleton'
 import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
 
 export default function SearchLoading() {
   const searchParams = useSearchParams()
   const query = searchParams.get('q')
 
   return (
-    <Suspense fallback={<div>Carregando...</div>}>
-      <div className="flex flex-col gap-4">
-        <p className="text-sm">
-          Resultados para: <span className="font-semibold">{query}</span>
-        </p>
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
 
-        <div className="grid grid-cols-3 gap-6">
-          {Array.from({ length: 9 }).map((_, index) => {
-            return <Skeleton key={index} className="h-[400px]" />
-          })}
-        </div>
+      <div className="grid grid-cols-3 gap-6">
+        {Array.from({ length: 9 }).map((_, index) => {
+          return <Skeleton key={index} className="h-[400px]" />
+        })}
       </div>
-    </Suspense>
+    </div>
   )
 }
